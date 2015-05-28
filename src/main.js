d7b21a2f9f18ac18da0cebae5874f4606e88e082
arkanciscan <arkanciscan@gmail.com>
2015-05-28 04:59:33 -0400

    cycle works!

--- src/main.js
+++ src/main.js
@@ -8,28 +8,21 @@ function app(events) {
 }
 
 function actions(events) {
-  console.log('actions');
   return {
     login: events.get('button#login', 'click')
   };
 }
 
 function state(actions) {
-  console.log('state');
-  
   return {
     mode: actions.login.map(function (event) {
-      console.log('mode changed');
       return 'login';
     })
   };
 }
 
 function render(state) {
-  console.log('render');
-  
   return state.mode.startWith('').map(function (mode) {
-    console.log(`mode: ${mode}`);
     return h('div', [
       h('button#login', 'Login'),
       mode == 'login' ? h('section.modal', [
